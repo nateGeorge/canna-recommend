10e3dbccd06d1eb2023c0fc01a606b0666980cf9
wordsforthewise <wordsforthewise@gmail.com>
2016-11-09 16:25:50 -0700

    FEAT: start parsing cannabinoids

--- analytical360/scrape_360.py
+++ analytical360/scrape_360.py
@@ -170,99 +170,181 @@ flow_df = get_flower_df(rows)
 # fsoup = bs(res1.content, 'lxml')
 # ims = fsoup.findAll('img')#, {'class':'szg-main-photo szg-show'})
 
-driver = setup_driver()
-driver.get(MAIN_URL)
-headers, cooks = get_headers_cookies(driver)
-base_im_path = 'images/'
-if not os.path.exists(base_im_path):
-    os.mkdir(base_im_path)
-# pages that aren't really flowers, but concentrates
-# others that have broken images links
-black_list = set(['http://analytical360.com/m/flowers/604216', 'http://analytical360.com/m/flowers/550371'])
-name_black_list = set(['Raw Pulp CJ',
-                        'Batch 35 Spent Trim',
-                        'B21 Spent Trim (CBD)',
-                        'B21 CBD',
-                        'B22 Spent Trim (THC)',
-                        'ACDC x Bubster #14 Male',
-                        'ACDC x Bubster #47 Male',
-                        'Blue Dog #19 Male',
-                        'Blue Dog #31 Male',
-                        'Canna-Tsu #16 Male',
-                        'Canna-Tsu #19 Male',
-                        'Foo Dog #3 Male',
-                        'Foo Dog #11 Male',
-                        'Foo Dog #12 Male',
-                        'Harle-Tsu #2 Male',
-                        'Harle-Tsu #7 Male',
-                        'Miami Blues #24',
-                        'Swiss Gold #6 Male',
-                        'Swiss Gold #18 Male',
-                        'Swiss Gold #26 Male',
-                        'Under Foo #8 Male',
-                        'Under Foo #11 Male',
-                        'Under Foo #27 Male',
-                        'Under Foo #35 Male',
-                        'Harle-Tsu #7Male'])
+def scrape_site():
+    driver = setup_driver()
+    driver.get(MAIN_URL)
+    headers, cooks = get_headers_cookies(driver)
+    base_im_path = 'images/'
+    if not os.path.exists(base_im_path):
+        os.mkdir(base_im_path)
+    # pages that aren't really flowers, but concentrates
+    # others that have broken images links
+    black_list = set(['http://analytical360.com/m/flowers/604216', 'http://analytical360.com/m/flowers/550371'])
+    name_black_list = set(['Raw Pulp CJ',
+                            'Batch 35 Spent Trim',
+                            'B21 Spent Trim (CBD)',
+                            'B21 CBD',
+                            'B22 Spent Trim (THC)',
+                            'ACDC x Bubster #14 Male',
+                            'ACDC x Bubster #47 Male',
+                            'Blue Dog #19 Male',
+                            'Blue Dog #31 Male',
+                            'Canna-Tsu #16 Male',
+                            'Canna-Tsu #19 Male',
+                            'Foo Dog #3 Male',
+                            'Foo Dog #11 Male',
+                            'Foo Dog #12 Male',
+                            'Harle-Tsu #2 Male',
+                            'Harle-Tsu #7 Male',
+                            'Miami Blues #24',
+                            'Swiss Gold #6 Male',
+                            'Swiss Gold #18 Male',
+                            'Swiss Gold #26 Male',
+                            'Under Foo #8 Male',
+                            'Under Foo #11 Male',
+                            'Under Foo #27 Male',
+                            'Under Foo #35 Male',
+                            'Harle-Tsu #7Male'])
 
-# broke here first time thru
-startrow = flow_df[flow_df['name'] == 'Mango Haze'].index[0]
-df_remain = flow_df.iloc[startrow:, :]
+    # broke here first time thru
+    startrow = flow_df[flow_df['name'] == 'Mango Haze'].index[0]
+    df_remain = flow_df.iloc[startrow:, :]
 
-cannabinoids = []
-terpenes = []
-im_sources = []
-no_imgs = []
-names = []
-for r in df_remain.iterrows():
-    i = r[0]
-    r = r[1]
-    link = r['link']
-    if link in black_list or r['name'] in name_black_list or re.search('.*\smale.*', r['name'], re.IGNORECASE) is not None or re.search('.*spent\s+trim.*', r['name'], re.IGNORECASE) is not None:
-        continue
+    cannabinoids = []
+    terpenes = []
+    im_sources = []
+    no_imgs = []
+    names = []
+    for r in df_remain.iterrows():
+        i = r[0]
+        r = r[1]
+        link = r['link']
+        if link in black_list or r['name'] in name_black_list or re.search('.*\smale.*', r['name'], re.IGNORECASE) is not None or re.search('.*spent\s+trim.*', r['name'], re.IGNORECASE) is not None:
+            continue
 
-    names.append(r['name'])
-    driver.get(link)
-    print link
-    try:
-        img = driver.find_element_by_xpath('//*[@id="mainwrapper"]/div[4]/div[1]/div[5]/div/div[1]/img[1]')
-        src = img.get_attribute('src')
-        im_sources.append(src)
-        print src
-    except:
-        no_imgs.append(r)
+        names.append(r['name'])
+        driver.get(link)
+        print link
+        try:
+            img = driver.find_element_by_xpath('//*[@id="mainwrapper"]/div[4]/div[1]/div[5]/div/div[1]/img[1]')
+            src = img.get_attribute('src')
+            im_sources.append(src)
+            print src
+        except:
+            no_imgs.append(r)
 
-    table1 = driver.find_element_by_xpath('//*[@id="mainwrapper"]/div[4]/div[1]/div[7]/div/div[1]/ul')
-    table1soup = bs(table1.get_attribute('innerHTML'))
-    table1rows = [l.get_text() for l in table1soup.findAll('li')]
-    cannabinoids.append(table1rows)
-    try:
-        table2 = driver.find_element_by_xpath('//*[@id="mainwrapper"]/div[4]/div[1]/div[8]/div/div/ul')
-    except:
-        table2 = driver.find_element_by_xpath('//*[@id="mainwrapper"]/div[4]/div[1]/div[9]/div/div/ul')
+        table1 = driver.find_element_by_xpath('//*[@id="mainwrapper"]/div[4]/div[1]/div[7]/div/div[1]/ul')
+        table1soup = bs(table1.get_attribute('innerHTML'))
+        table1rows = [l.get_text() for l in table1soup.findAll('li')]
+        cannabinoids.append(table1rows)
+        try:
+            table2 = driver.find_element_by_xpath('//*[@id="mainwrapper"]/div[4]/div[1]/div[8]/div/div/ul')
+        except:
+            table2 = driver.find_element_by_xpath('//*[@id="mainwrapper"]/div[4]/div[1]/div[9]/div/div/ul')
 
-    table2soup = bs(table2.get_attribute('innerHTML'))
-    table2rows = [l.get_text() for l in table1soup.findAll('li')]
-    terpenes.append(table2rows)
+        table2soup = bs(table2.get_attribute('innerHTML'))
+        table2rows = [l.get_text() for l in table1soup.findAll('li')]
+        terpenes.append(table2rows)
 
-    clean_name = re.sub('/', '-', r['name'])
-    save_path = base_im_path + clean_name
-    if os.path.exists(save_path):
-        save_path = save_path + i
+        clean_name = re.sub('/', '-', r['name'])
+        save_path = base_im_path + clean_name
+        if os.path.exists(save_path):
+            save_path = save_path + i
 
-    save_path = save_path + '.jpg'
-    print save_path
+        save_path = save_path + '.jpg'
+        print save_path
 
-    download_image(src, save_path, headers, cooks)
+        download_image(src, save_path, headers, cooks)
 
-pk.dump(cannabinoids, open('cannabinoids.pk', 'w'), 2)
-pk.dump(terpenes, open('terpenes.pk', 'w'), 2)
-pk.dump(no_imgs, open('no_imgs.pk', 'w'), 2)
-pk.dump(im_sources, open('im_sources.pk', 'w'), 2)
-pk.dump(names, open('names.pk', 'w'), 2)
+def save_raw_scrape(cannabinoids, terpenes, no_imgs, im_sources, names):
+    pk.dump(cannabinoids, open('analytical360/cannabinoids.pk', 'w'), 2)
+    pk.dump(terpenes, open('analytical360/terpenes.pk', 'w'), 2)
+    pk.dump(no_imgs, open('analytical360/no_imgs.pk', 'w'), 2)
+    pk.dump(im_sources, open('analytical360/im_sources.pk', 'w'), 2)
+    pk.dump(names, open('analytical360/names.pk', 'w'), 2)
 
-testdf = pd.DataFrame({'name':names})
-testdf['name'].value_counts()[testdf['name'].value_counts() > 1]
+def load_raw_scrape():
+    cannabinoids = pk.load(open('analytical360/cannabinoids.pk'))
+    terpenes = pk.load(open('analytical360/terpenes.pk'))
+    no_imgs = pk.load(open('analytical360/no_imgs.pk'))
+    im_sources = pk.load(open('analytical360/im_sources.pk'))
+    names = pk.load(open('analytical360/names.pk'))
+    return cannabinoids, terpenes, no_imgs, im_sources, names
+
+def parse_raw_scrape(cannabinoids, terpenes, names):
+    trail = [0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1]
+    cannabinoid_strs = ['thc-a', 'thc', 'cbn', 'thc total', 'thc-total', 'cbd-a', 'cbd', 'cbd-total', 'cbd total', 'cbg', 'cbc', 'activated total', 'activated', 'active']
+    c_dict_keys = ['thca', 'thc', 'cbn', 'thc_total', 'cbda', 'cbd', 'cbd_total', 'cbg', 'cbc', 'activated_total']
+    conversion_dict = {'thc-a':'thca',
+                        'thc total':'thc_total',
+                        'thc-total':'thc_total',
+                        'cbd-a':'cbda',
+                        'cbd-total':'cbd_total',
+                        'cbd total':'cbd_total',
+                        'activated total':'activated_total',
+                        'activated':'activated_total'} # converts similar strings to the dict key forf cannabiniod dict
+    cannabinoid_dict = {}
+    for t, c in zip(trail, cannabinoid_strs):
+        has_str, num = find_str(cannabinoids, c, t)
+        if has_str:
+            pass
+
+        print c, np.mean(has_str)
+
+
+    thca = []
+    thc = []
+    total_thc = []
+    cbn = []
+    cbda = []
+    cbd = []
+    total_cbd = []
+    cbg = []
+    cbc = []
+    activated_total = []
+
+def find_string(search_str, str_to_find='THC-A', trail=True):
+    if trail:
+        find_str = '.*' + str_to_find + '.*'
+    else:
+        find_str = '.*' + str_to_find
+    has_str = 0
+    res = re.search(find_str, j, re.IGNORECASE)
+    if res:
+        num = re.search('[\d\.]*').group(0)
+        return 1, num
+
+    return 0, 0
+
+def check_for_string(cannabinoids, str_to_find='THC-A', trail=True):
+    if trail:
+        find_str = '.*' + str_to_find + '.*'
+    else:
+        find_str = '.*' + str_to_find
+    #c = [' '.join(r) for r in cannabinoids]
+    has_str = []
+    for c in cannabinoids:
+        has_str_val = 0
+        for j in c:
+            res = re.search(find_str, j, re.IGNORECASE)
+            if res:
+                has_str_val = 1
+                break
+
+        has_str.append(has_str_val)
+
+    return has_str
+
+def check_if_fields_present():
+    trail = [0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1]
+    cannabinoid_strs = ['thc-a', 'thc', 'cbn', 'thc total', 'thc-total', 'cbd-a', 'cbd', 'cbd-total', 'cbd total', 'cbg', 'cbc', 'activated total', 'activated', 'active']
+    for t, c in zip(trail, cannabinoid_strs):
+        has_str = check_for_string(cannabinoids, c, t)
+        print c, np.mean(has_str)
+
+def stuff():
+    testdf = pd.DataFrame({'name':names})
+    testdf['name'].value_counts()[testdf['name'].value_counts() > 1]
 
 # for finding the images on the page with 'uploads' in the path
 # uploads = []
