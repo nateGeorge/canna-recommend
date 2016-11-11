938b541f1b364ed0d60a7af4e62af0e5287b92a0
wordsforthewise <wordsforthewise@gmail.com>
2016-11-10 19:03:03 -0700

    FEAT: start scraping archives

---
+++ analytical360/scrape_360_archives.py
@@ -0,0 +1,37 @@
+import requests
+from bs4 import BeautifulSoup as bs
+from selenium import webdriver
+from fake_useragent import UserAgent
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+import pandas as pd
+import numpy as np
+import re
+import os
+import cPickle as pk
+
+ua = UserAgent()
+
+def setup_driver():
+    dcap = dict(DesiredCapabilities.PHANTOMJS)
+    dcap["phantomjs.page.settings.userAgent"] = (ua.random)
+    driver = webdriver.PhantomJS(desired_capabilities=dcap)
+    driver.set_window_size(1920, 1080)
+    return driver
+
+def get_headers_cookies(driver):
+    agent = ua.random  # select a random user agent
+    headers = {
+        "Connection": "close",  # another way to cover tracks
+        "User-Agent": agent
+    }
+    cookies = driver.get_cookies()
+    cooks = {}
+    for c in cookies:
+        cooks[c['name']] = c['value']  # map it to be usable for requests
+
+    return headers, cooks
+
+# using requests: finding it hard to get all the correct entries
+MAIN_URL = 'http://analytical360.com/m/archived'
+res = requests.get(MAIN_URL)
+soup = bs(res.content, 'lxml')
